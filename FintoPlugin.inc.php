2e42c05f7469968fdf1898857dcf0bd123f68eec
ajnyga <ajnyga@gmail.com>
2019-11-06 20:58:22 +0200

    Fix output unRegisterFilter call and wait for the right template

--- FintoPlugin.inc.php
+++ FintoPlugin.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file fintoPlugin.inc.php
  *
- * Copyright (c) 2013-2017 Simon Fraser University Library
- * Copyright (c) 2003-2017 John Willinsky
+ * Copyright (c) 2013-2019 Simon Fraser University Library
+ * Copyright (c) 2003-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class fintoPlugin
@@ -29,61 +29,56 @@ class FintoPlugin extends GenericPlugin {
     function getDescription() {
         return "Integrates YSO ontology from the Finnish Finto service to the keyword field in OJS3. Supports Finnish, English and Swedish.";
     }
-	
-    function register($category, $path) {
+
+    function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
-				HookRegistry::register ('TemplateManager::include', array(&$this, 'handleTemplateDisplay'));				
+				HookRegistry::register('TemplateResource::getFilename', array($this, 'handleFormDisplay'));
         }
-		return $success;    
+		return $success;
 	}
-	
-	/**
-	 * @see TemplateManager::display()
-	 */
-	function handleTemplateDisplay($hookName, $args) {
-		$templateMgr =& $args[0];
-		$params =& $args[1];
-		$request =& PKPApplication::getRequest();
 
-		if (!isset($params['smarty_include_tpl_file'])) return false;
-
-		switch ($params['smarty_include_tpl_file']) {
+	function handleFormDisplay($hookName, $args) {
+		$request = PKPApplication::getRequest();
+		$templateMgr = TemplateManager::getManager($request);
+		$template =& $args[1];
+		switch ($template) {
 			case 'submission/submissionMetadataFormFields.tpl':
-				$templateMgr->register_outputfilter(array($this, 'keywordsFilter'));
+				$templateMgr->registerFilter("output", array($this, 'keywordsFilter'));
 				break;
 		}
 		return false;
-	}	
-	
+	}
+
 	/**
-	 * Output filter adds Finto to supporting agencies fields by overriding the existing controlled vocabulary settings
+	 * Output filter adds Finto to keyword fields by overriding the existing controlled vocabulary settings
 	 * @param $output string
 	 * @param $templateMgr TemplateManager
 	 * @return $string
 	 */
-	function keywordsFilter($output, &$templateMgr) {
-		
+	function keywordsFilter($output, $templateMgr) {
+
 		$endPoint = '</script>';
-		
+
 		// en_US
 		$startPoint = 'en_US-keywords\]\[\]",';
 		$newscript = $this->ysoTagit('en');
 		$output = preg_replace('#('.$startPoint.')(.*?)('.$endPoint.')#si', '$1'.$newscript.'$3', $output, 1);
-		
+
 		// fi_FI
 		$startPoint = 'fi_FI-keywords\]\[\]",';
 		$newscript = $this->ysoTagit('fi');
-		$output = preg_replace('#('.$startPoint.')(.*?)('.$endPoint.')#si', '$1'.$newscript.'$3', $output, 1);		
-		
+		$output = preg_replace('#('.$startPoint.')(.*?)('.$endPoint.')#si', '$1'.$newscript.'$3', $output, 1);
+
 		// sv_SE
 		$startPoint = 'sv_SE-keywords\]\[\]",';
 		$newscript = $this->ysoTagit('sv');
 		$output = preg_replace('#('.$startPoint.')(.*?)('.$endPoint.')#si', '$1'.$newscript.'$3', $output, 1);		
-		
-		
-		$templateMgr->unregister_outputfilter('keywordsFilter');
-		
+
+		if (stristr($output, '-keywords][]')){
+			$templateMgr->unregisterFilter('output', array($this, 'keywordsFilter'));
+		}
+
 		return $output;
 	}
 	
@@ -96,7 +91,7 @@ class FintoPlugin extends GenericPlugin {
 		return "allowSpaces: true,
 				tagSource: function(request, response){
 						$.ajax({
-							url: 'https://api.finto.fi/rest/v1/search?vocab=yso&lang=".$locale."',
+							url: 'https://api.finto.fi/rest/v1/search?vocab=yso&lang=" . $locale . "',
 							dataType: 'json',
 							data: {
 								query: request.term + '*'
@@ -118,10 +113,5 @@ class FintoPlugin extends GenericPlugin {
 
 		});";
 	}
-	
-	
-	
-	
-   
 }
 ?>
